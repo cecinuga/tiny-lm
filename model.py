b078c2a4e2132082c9041b0e94a701adac3c8f16
cecinuga <cecinuga.tdb@gmail.com>
2026-05-08 00:18:19 +0200

    developed forward function

--- model.py
+++ model.py
@@ -21,3 +21,26 @@ class GPT(nn.Module):
         ))
         self.lm_head = nn.Linear(config.n_emdb, config.vocab_size, bias=False)
         self.transformer.wte.weight = self.lm_head.weight
+
+    def forward(self, idx, targets=None):
+        B, T = idx.shape
+        pos = torch.arange(0, T, device=idx.device)
+
+        tok_emb = self.transformer.wte(idx) # (B, T, n_embd)
+        pos_emb = self.transformer.wpe(pos) # (T, n_embd)
+        x = tok_emb + pos_emb # (B, T, n_embd) — broadcasting adds position info
+
+        for block in self.transformer.h:
+            x = block(x)
+
+        x = self.transformer.ln_f(x)
+        logits = self.lm_head(x)
+
+        loss = None
+        if targets is not None:
+            loss = nn.functional.cross_entropy(
+                logits.view(-1, logits.size(-1)),
+                targets.view(-1)
+            )
+
+        return logits, loss
