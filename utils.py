a630695faf80d25d738bf4f6689b4cc513a5cf04
cecinuga <cecinuga.tdb@gmail.com>
2026-05-23 04:27:46 +0200

    incapsulated model_arch function

--- utils.py
+++ utils.py
@@ -3,8 +3,11 @@ import time
 from model import GPTConfig, GPT
 today = time.strftime("%Y%m%d")
 
+def model_arch(config: GPTConfig):
+    return f"L{config.n_layer}H{config.n_head}E{config.n_embd}"
+
 def checkpoint_name(step, config:GPTConfig, date=today, prefix="final"):
-    return f"{prefix}_{today}_L{config.n_layer}H{config.n_head}E{config.n_embd}_{step}"
+    return f"{prefix}_{today}_{model_arch(config)}_{step}"
 
 def save_checkpoint(model: GPT, config: GPTConfig, step, stoi, itos, prefix="checkpoint"):
     torch.save(
